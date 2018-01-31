d5a0af59cacb71ba2c3369e8a678e102ce4e5e9d
Alex <avlonitis@msn.com>
2018-01-31 20:51:51 +0000

    simple react youtube player

--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import _ from 'lodash';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSeach from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'YOUR YOUTUBE API KEY';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+    this.fetchVideos('cats')
+  }
+
+  fetchVideos(searchTerm) {
+    YTSeach({key: API_KEY, term: searchTerm}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
+    });
+  }
+
+  render() {
+    const fetchVideos = _.debounce((searchTerm) => { this.fetchVideos(searchTerm) }, 300)
+
+    return (
+      <div>
+        <SearchBar
+          onSearchTermChange={fetchVideos}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+const container = document.querySelector('.container')
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+  <App />,
+  container
+);
